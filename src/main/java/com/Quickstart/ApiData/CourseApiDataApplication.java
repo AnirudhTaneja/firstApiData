4edf92856d14aa6abb3377d08e4304b18931235d
AnirudhTaneja <Anirudh.Taneja@target.com>
2020-05-25 20:12:25 -0400

    initial committ

---
+++ src/main/java/com/Quickstart/ApiData/CourseApiDataApplication.java
@@ -0,0 +1,14 @@
+package com.Quickstart.ApiData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("topic")
+@SpringBootApplication
+public class CourseApiDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiDataApplication.class,args);
+
+    }
+}
